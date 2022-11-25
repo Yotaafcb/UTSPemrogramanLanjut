d662ee578f0bb46cb68000a68caf26c56fc976ce
AnggaYota_21.11.4517 <94093717+Yotaafcb@users.noreply.github.com>
2022-11-25 13:59:31 +0700

    Add files via upload

---
+++ layout2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PemrogLanjut
+{
+    /// <summary>
+    /// Interaction logic for layout2.xaml
+    /// </summary>
+    public partial class layout2 : Window
+    {
+        public layout2()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            layout3 layout3 = new layout3();
+            this.Content = layout3;
+        }
+    }
+}
---
+++ layout3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PemrogLanjut
+{
+    /// <summary>
+    /// Interaction logic for layout3.xaml
+    /// </summary>
+    public partial class layout3 : Page
+    {
+        public layout3()
+        {
+            InitializeComponent();
+        }
+    }
+}
